1753a92692b277fccf6efc8cc5797c928227f231
Jonas Hietala <tradet.h@gmail.com>
2012-11-12 20:58:06 +0100

    Gonna minimize simplebot.

---
+++ minibot.pl
@@ -0,0 +1,66 @@
+use Modern::Perl;
+use IO::Socket;
+
+# Configs
+my $server = "irc.quakenet.org";
+my $port = 6667;
+my $nickname = "simplebot";
+my $username = "simplebot";
+my $realname = "I'm very simple minded oh yeah";
+
+# Our socket we use to communicate with
+my $sock;
+
+# Output something to the socket and to stdout
+sub output
+{
+    my $msg = join (" ", @_);
+    print $sock "$msg\r\n";
+    say "> $msg";
+}
+
+sub o{print $sock "$_[0]\r\n"; say "> $_[0]";}
+
+# Connect to the IRC server
+$sock=new IO::Socket::INET(PeerAddr=>$server,PeerPort=>$port);
+
+# Register user and nickname
+o("NICK $nickname");
+o("USER $username 0 * :$realname");
+
+# Parse input
+while (my $input = <$sock>) {
+    print "< $input";
+
+    # Handle ping
+    if ($input =~ /^PING\s(.*)$/i) {
+        output ("PONG $1");
+    }
+
+    # Split irc message into parts
+    if ($input =~ /^
+        (?:
+            :(\S+)      # (1) prefix
+            \s
+        )?              # prefix isn't mandatory
+        (\S+)           # (2) cmd
+        \s
+        (.+?)           # (3) parameters
+        \r?             # irc standard includes carriage return which we don't want
+        $
+        /x) {
+
+        my ($prefix, $cmd, $param) = ($1 || "", $2, $3);
+
+        # Join channel when connected
+        if ($cmd =~ /004/) {
+            output ("JOIN #madeoftree");
+        }
+        # Handle a privmsg and if someone says hello, respond
+        elsif ($cmd eq "PRIVMSG" && $param =~ /^(#.+) :.*hello/) {
+            my $channel = $1;
+            output ("PRIVMSG $channel :hello there!");
+        }
+    }
+}
+
